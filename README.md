339e2f616314764f0ff93fa1cbd73f9ae9c9cc43
Ni Ngo <ni.ngo@asnet.com.vn>
2019-11-27 18:06:00 +0700

    Configure webpack and customize tailwind

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Tailwind css
-Gat started with Tailwind css
+# Tailwind css: Get started
+## How to run sample:
+1. Clone source code:
+```
+git clone git@github.com:ningo-agilityio/tailwind-css.git
+```
+
+2. Install dependencies:
+```
+yarn install
+```
+
+3. Run sample
+```
+yarn run build
+```
+
+## Target:
+- Configure to build tailwind by webpack
+- Get familiar to define tailwind configuration
+- Get familiar to customize theme, add components
+- Get familiar to build your own classes
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/styles.css',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: [
+                  require('tailwindcss'),
+                  require('autoprefixer'),
+                ],
+              },
+            }
+          ]
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+}
